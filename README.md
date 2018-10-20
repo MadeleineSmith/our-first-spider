7dc1c5f3183614a16fd7ade2495c52b8d07bec5e
david1707 <DavidAlacant@gmail.com>
2018-10-20 12:24:17 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# our-first-spider
+Created for my Scrapy tutorial, a Scrapy bot that scrapes quotes from quotes.toscrape.com
---
+++ quotes_toscrape/spiders/main_spider.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import scrapy                                       
+
+
+class MainSpiderSpider(scrapy.Spider):                  
+    name = 'main_spider'                                
+    allowed_domains = ['quotes.toscrape.com']           
+    start_urls = [
+        'http://quotes.toscrape.com/',
+    ]
+
+    def parse(self, response):
+        quotes = response.xpath('//div[@class="quote"]')
+
+        for quote in quotes:
+            text = quote.xpath('./span[@class="text"]/text()').extract_first()
+            author = quotes.xpath('.//small[@class="author"]/text()').extract_first()
+            tags = quote.xpath('.//div[@class="tags"]/a/text()').extract()
+
+            yield {
+                'Quote': text,
+                'Author': author,
+                'Tags': tags
+            }
